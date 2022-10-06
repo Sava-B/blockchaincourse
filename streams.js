8c4f9720860d7cb8c076422ab1564ce0016b9ebb
Sava <savab2164@gmail.com>
2022-10-05 21:34:38 -0600

    edit

--- streams.js
+++ streams.js
@@ -1,3 +1,9 @@
 const fs = require('fs');
 
-fs.createReadStream()
+const readStream = fs.createReadStream('./sample-2mb-text-file.txt','utf-8');
+
+// const writeStream = fs.createWriteStream('example2.txt');
+
+readStream.on('data',(chunk) => {
+    console.log(chunk);
+});

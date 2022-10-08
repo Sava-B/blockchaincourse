86f6f1996c6adce17bdae999dee1189cd9f18eb1
Sava <savab2164@gmail.com>
2022-10-07 21:28:48 -0600

    edit

---
+++ startexpress.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use('/public',express.static(path.join(__dirname,'static')));
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Using route params
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'static','index.html'));
+});
+
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    // database work here
+    res.send('successfully posted data');
+})
+
+
+
+app.listen(3000);

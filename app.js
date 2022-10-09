0d61c9063ce9af954e9152edad5d4881b60d036e
Sava-B <savab2164@gmail.com>
2022-10-08 19:00:28 -0600

    edit

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.use('/public',express.static(path.join(__dirname,'static')));
 app.set('view engine','ejs');
-app.get('/',(req,res)=>{
-    res.render('index');
+app.get('/:userQuery',(req,res)=>{
+    res.render('index',{data : {userQuery: req.params.userQuery,
+                        searchResults : ['book1', 'book2', 'book3']}});
 });
-
 
 app.listen(3000);

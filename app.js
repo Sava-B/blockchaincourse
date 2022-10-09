3cfd48976c6dba203101f3859d8713b11109d1c1
Sava <savab2164@gmail.com>
2022-10-08 21:35:46 -0600

    edit

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// app.use(bodyparser.json());
+// app.use((req, res, next) => {
+//     console.log(req.url,req.method);
+//     req.banana = 'banana'
+//     next();
+// })
+
+// app.get('/',(req,res)=>{
+//     console.log(req.banana)
+//     res.send('MiddleWare');
+// })
+
+
+// using express routes
+
 app.use('/public',express.static(path.join(__dirname,'static')));
 app.set('view engine','ejs');
+
+const people = require('./routes/people');
+
+app.use('/people',people);
+
 app.get('/:userQuery',(req,res)=>{
     res.render('index',{data : {userQuery: req.params.userQuery,
                         searchResults : ['book1', 'book2', 'book3'],

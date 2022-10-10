4c3eb4350230c85757710f887199b71439ae2869
Sava <savab2164@gmail.com>
2022-10-09 22:29:44 -0600

    edit

---
+++ deploy.js
@@ -0,0 +1,29 @@
+const ethers = require("ethers");
+const fs = require("fs");
+
+// http://127.0.0.1:7545
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:7545"
+  );
+  const wallet = new ethers.Wallet(
+    "31e68ea5ea1efe756b6a79a503960c1b29d7dfa46ac073f42c77d5674ca5baed",
+    provider
+  );
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait...");
+  const contract = await contractFactory.deploy(); // Stop here! Wait for contract to deploy
+  console.log(contract);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });

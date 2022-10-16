da2929e24a3e1952d924f173869491f33096f401
Sava <savab2164@gmail.com>
2022-10-15 21:16:24 -0600

    edit

---
+++ hardhat-simple-storage/scripts/deploy.js
@@ -0,0 +1,41 @@
+// imports
+const { ethers, run, network } = require("hardhat");
+
+// async main
+async function main() {
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    );
+    console.log("Deploying contract...");
+    const simpleStorage = await SimpleStorageFactory.deploy();
+    await simpleStorage.deploy;
+    console.log(`Deployed contract to ${simpleStorage.address}`);
+    if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.deployTransaction.wait(6);
+        await verify(simpleStorage.address, []);
+    }
+}
+
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...");
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        });
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!");
+        } else {
+            console.log(e);
+        }
+    }
+}
+
+// main
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
